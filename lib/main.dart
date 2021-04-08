73789c201a1cf2c0082fb3a8267f2b8f64b511cc
balvinderz <balvindersi2@gmail.com>
2021-04-08 14:15:16 +0530

    home page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:pokedex_new/fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,11 +31,74 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PokedexHomePage extends StatelessWidget
-{
+class PokedexHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: HexColor("#C82632"),
+      body: Stack(
+        children: [
+          ClipRRect(
+            child: Align(
+              heightFactor:1,
+                // alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding:  EdgeInsets.only(top: MediaQuery.of(context).size.height*0.2),
+                  child: Image.asset("assets/ash.png"),
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal :20.0,vertical: 50),
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                        colors: [
+                          Color.fromRGBO(255, 255, 255, 0.35),
+                          Color.fromRGBO(233, 233, 233, 0.14)
+                        ],
+                        begin: FractionalOffset(0.0, 0.0),
+                        end: FractionalOffset(0.5, 0.0),
+                        stops: [0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                  ),
+                  child: TextButton(
+                    child:Text("Start".toUpperCase(),style: GoogleFonts.heebo(
+          fontSize: 20,
+
+          fontWeight: FontWeight.bold ,
+          color: HexColor("#343434")
+        ),),
+                    onPressed: () {
+                      // askPermission();
+                    },
+                  )),
+            ),
+          ) ,
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Text("Welcome to ",style: heeboFont.copyWith(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 16,
+                  color: Colors.white
+                ),),
+                Text("Pokedex",style: heeboFont.copyWith(
+                  fontSize: 62,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+                ),),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
